e867a085fb8b079a3c9cb63677149af134e5b6a2
Brian Bice <b.bice23@gmail.com>
2015-08-14 14:57:28 -0700

    Refactored DonutShop object. Added DonutMaster object

--- donuts.js
+++ donuts.js
@@ -1,13 +1,13 @@
-var Donut = function(location, minCust, maxCust, avgDonuts, hours) {
-  this.location = location,
-  this.minCust = minCust,
-  this.maxCust = maxCust,
-  this.avgDonuts = avgDonuts,
-  this.hours = hours,
+var DonutShop = function(location, minCust, maxCust, avgDonuts, hours) {
+  this.location = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgDonuts = avgDonuts;
+  this.hours = hours;
 
   this.numDonutsPerHour = function() {
     return Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust) * this.avgDonuts);
-  },
+  };
 
   this.numDonutsPerDay = function() {
     var totalDonuts = 0;
@@ -18,16 +18,32 @@ var Donut = function(location, minCust, maxCust, avgDonuts, hours) {
   };
 };
 
-var downtown = new Donut('Downtown', 8, 43, 4.5, 12),
-    capitolHill = new Donut('Capitol Hill', 4, 37, 2, 9),
-    southLakeUnion = new Donut('South Lake Union', 9, 23, 6.33, 8),
-    wedgewood = new Donut('Wedgewood', 2, 28, 1.25, 18),
-    ballard = new Donut('Ballard', 8, 58, 3.75, 18);
+var DonutMaster = function() {
+  this.shops = [];
 
-console.log(downtown.numDonutsPerDay());
-console.log(capitolHill.numDonutsPerDay());
-console.log(southLakeUnion.numDonutsPerDay())
-console.log(wedgewood.numDonutsPerDay())
-console.log(ballard.numDonutsPerDay())
+  this.addShop = function(location, minCust, maxCust, avgDonuts, hours)  {
+      this.shops.push(new DonutShop(location, minCust, maxCust, avgDonuts, hours))
+  };
 
-// Was having a terrible time figuring out how to run the for loop. Ended up finding some code on GitHub that helped me out, from a previous CodeFellows student. The function is still set up in my own fashion, tho.
+  this.generateReport = function() {
+    var report = '';
+    for (var i = 0; i < this.shops.length; i++) {
+      report += this.shops[i].location +' location will sell ' + this.shops[i].numDonutsPerHour() + ' donuts in an hour or ' + this.shops[i].numDonutsPerDay() + ' donuts in a day. ';
+    };
+    return report;
+  };
+};
+
+var topPot = new DonutMaster();
+
+var dunkinDonuts = new DonutMaster();
+
+var krispyKreme = new DonutMaster();
+
+dunkinDonuts.addShop('Downtown', 4, 79, 6, 12);
+
+topPot.addShop('Downtown', 8, 43, 4.5, 12);
+topPot.addShop('Capitol Hill', 4, 37, 2, 9);
+topPot.addShop('South Lake Union', 9, 23, 6.33, 8);
+topPot.addShop('Wedgewood', 2, 28, 1.25, 18);
+topPot.addShop('Ballard', 8, 58, 3.75, 18);
